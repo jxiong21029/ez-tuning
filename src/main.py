e7f9c57fbff6f4e971dc207a97a8ff6cb2ce7a36
Jerry Xiong <jxiong60@gatech.edu>
2023-07-04 00:33:58 -0400

    Add checks for correct args

--- src/main.py
+++ src/main.py
@@ -3,5 +3,20 @@ import sys
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("usage: plotgen [file]")
+        return
+
     path = os.path.join(os.path.split(__file__)[0], "plotting.py")
-    os.system(f"bokeh serve {path} --args {sys.argv[1]}")
+    filename = sys.argv[1]
+
+    if not os.path.exists(filename):
+        print(f"plotgen: {filename}: No such file or directory")
+        return
+    if os.path.isdir(filename):
+        print(f"plotgen: {filename}: Is a directory")
+        return
+
+    assert os.path.exists(path)  # this should be correct regardless of input
+
+    os.system(f"bokeh serve {path} --args {filename}")
